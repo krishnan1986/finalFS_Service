10a1a8825c38c0c1a34159908d5a39502b2d0e41
krishnanc1986 <krishnanc.in@gmail.com>
2019-10-29 18:40:33 +0530

    adding controller

--- src/main/java/com/finalFS/controller/BaseController.java
+++ src/main/java/com/finalFS/controller/BaseController.java
@@ -1,18 +1,21 @@
 package com.finalFS.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.finalFS.dao.DBOpsImpl;
 import com.finalFS.model.User;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping("/home")
 public class BaseController {
 	@Autowired
 	DBOpsImpl daoObj;
 
-	@PostMapping
+	@PostMapping(value="/addUser")
 	public void addUsertoDB(User user)
 	{
 		daoObj.addUser(user);
